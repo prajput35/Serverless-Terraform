8bec22bd0c45d2a656e22fc126703a317c5c80d0
Paras <Paras.code.api@gmail.com>
2020-07-07 22:09:28 +0530

    backend code updated

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -1,10 +1,10 @@
 terraform {
   
     backend "azurerm" {
-        storage_account_name = "__terraformstorageaccount__"
-        container_name       = "terraform"
-        key                  = "terraform.tfstate"
-	    access_key  ="__storagekey__"
+        #storage_account_name = "__terraformstorageaccount__"
+        #container_name       = "terraform"
+        #key                  = "terraform.tfstate"
+	    #access_key  ="__storagekey__"
   	}
 }
 provider "azurerm"{
